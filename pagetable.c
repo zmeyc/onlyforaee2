c50b0abe49c63dfa2ece7dbb7a1c6f5d3817cdb3
Yuhan Shao <yuhan.shao@mail.utoronto.ca>
2018-06-21 17:35:32 -0400

    Back

--- pagetable.c
+++ pagetable.c
@@ -179,8 +179,6 @@ char *find_physpage(addr_t vaddr, char type) {
 		p->frame = (unsigned int) new_frame << PAGE_SHIFT;
 		p->frame |= PG_ONSWAP;
 
-                p->swap_off = INVALID_SWAP;
-
 	} else {
 		hit_count++;
 	}
