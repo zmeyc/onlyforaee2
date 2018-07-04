c3df4555bd08b4c795bae35eda60de31fca607b3
Yuhan Shao <yuhan.shao@mail.utoronto.ca>
2018-07-04 14:29:45 -0400

    comment

--- opt.c
+++ opt.c
@@ -6,13 +6,13 @@
 #include "pagetable.h"
 #include "sim.h"
 
+#define INF -1
+
 extern unsigned memsize;
 
 extern int debug;
 
 extern struct frame *coremap;
-
-#define INF -1
 
 // Define data structures
 typedef struct page {
@@ -20,9 +20,15 @@ typedef struct page {
 	int next_ref; // Store index of the next reference of this page, -1 if none
 } Page;
 
+// Define the global variables
 Page *page_list;
 int time = 0;
 
+/*
+ * Return the index of the previous reference of the page with
+ * virtual address vaddr if it has the previous reference.
+ * Otherwise, return INF.
+ */
 int find_pre(addr_t vaddr, int index) {
 	int i = index - 1;
 	while (i >= 0) {
@@ -31,6 +37,7 @@ int find_pre(addr_t vaddr, int index) {
 		}
 		i--;
 	}
+
 	return INF;
 }
 
@@ -40,16 +47,18 @@ int find_pre(addr_t vaddr, int index) {
  */
 int opt_evict() {
 	int last_call = 0;
-	int index;
+	int index = 0;
 
 	for (int i = 0; i < memsize; i++) {
-		if (coremap[i].next_ref_time == INF) {
+		if (coremap[i].next_ref_time == INF) { // Never refer again, evict
 			return i;
 		} else if (coremap[i].next_ref_time > last_call) {
+			// Evict the page that won't be used for the longest period of time
 			last_call = coremap[i].next_ref_time;
 			index = i;
 		}
 	}
+
 	return index;
 }
 
@@ -58,10 +67,8 @@ int opt_evict() {
  * Input: The page table entry for the page that is being accessed.
  */
 void opt_ref(pgtbl_entry_t *p) {
-
+	//
 	coremap[p->frame >> PAGE_SHIFT].next_ref_time = page_list[time].next_ref;
-	// printf("%d\n", time);
-	// free(&page_list[time]);
 	time++;
 }
 
@@ -69,14 +76,15 @@ void opt_ref(pgtbl_entry_t *p) {
  * replacement algorithm.
  */
 void opt_init() {
-	int size = 0; // To find size of the tracefile to initialized page_list
-	int index = 0; // Record current index of the page inside tracefile
+	int size = 0; // Store the size of the tracefile to initialize the page_list
+	int index = 0; // Record the current index of the page inside tracefile
 	FILE *tfp = NULL;
 
 	char buf[MAXLINE];
 	addr_t vaddr = 0;
 	char type;
 
+	// Open the tracefile for reading
 	if(tracefile != NULL) {
 		if((tfp = fopen(tracefile, "r")) == NULL) {
 			perror("Error opening tracefile:");
@@ -84,39 +92,40 @@ void opt_init() {
 		}
 	}
 
+	// Keep track of the size of the tracefile
 	while(fgets(buf, MAXLINE, tfp) != NULL) {
 		if(buf[0] != '=') {
 			size++;
-		} else {
-			continue;
 		}
 	}
 
+	// Allocate the space for the page_list
 	page_list = malloc(sizeof(Page) * size);
-	fseek(tfp, 0, SEEK_SET); // Set to the beginning of the tracefile
+	// fseek(tfp, 0, SEEK_SET); // Set to the beginning of the tracefile
 
+	// Read the lines
 	while(fgets(buf, MAXLINE, tfp) != NULL) {
-
 		if(buf[0] != '=') {
 			sscanf(buf, "%c %lx", &type, &vaddr);
-			if(debug)  {
+			if (debug) {
 				printf("%c %lx\n", type, vaddr);
 			}
 
-		page_list[index].id = vaddr;
-		page_list[index].next_ref = INF;
+			// Store the information of each line
+			page_list[index].id = vaddr;
+			page_list[index].next_ref = INF;
 
-		int pre_ref = find_pre(vaddr, index);
-		if (pre_ref != INF) { // not first reference
-			page_list[pre_ref].next_ref = index;
-		}
+			// Keep track of the next reference
+			int pre_ref = find_pre(vaddr, index);
+			if (pre_ref != INF) { // Not the first reference
+				page_list[pre_ref].next_ref = index;
+			}
 
-		index++;
-		} else {
-			continue;
+			index++;
 		}
 	}
 
+	// Close the tracefile after reading
 	if(fclose(tfp) != 0) {
 		perror("Error opening tracefile:");
 		exit(1);
