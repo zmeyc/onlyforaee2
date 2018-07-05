4aabedc9b36491ef85e72505aa946cc014e89b1d
Yuhan Shao <yuhan.shao@mail.utoronto.ca>
2018-07-04 21:23:53 -0400

    fib

--- traceprogs/fibonacci.c
+++ traceprogs/fibonacci.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+  /* Markers used to bound trace regions of interest */
+  volatile char MARKER_START, MARKER_END;
+  /* Record marker addresses */
+  FILE* marker_fp = fopen("simpleloop.marker","w");
+  if(marker_fp == NULL ) {
+    perror("Couldn't open marker file:");
+    exit(1);
+  }
+  fprintf(marker_fp, "%p %p", &MARKER_START, &MARKER_END );
+  fclose(marker_fp);
+
   if (argc != 2) {
     printf("Please give a positive integer.\n");
     return -1;
   }
 
-  int num = argv[1];
+  long int num = strtol(argv[1], NULL, 10);
 
   int pre = 0;
   int result = 1;
